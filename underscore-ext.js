9d3815c7cd0f1cda8153e2fb67955c92270d8061
Jesse Tane <jesse.tane@gmail.com>
2012-05-08 15:13:07 -0700

    added some functions

--- underscore-ext.js
+++ underscore-ext.js
@@ -1,8 +1,9 @@
 //
 //
 //
+//
 
-_.extend(_, {
+_.mixin({
   
   log: function () {
     if (window.console) {
@@ -12,6 +13,32 @@ _.extend(_, {
   
   isMobileBrowser: function () {
     return navigator.appVersion.toLowerCase().search("mobile") > -1
+  },
+
+  // returns the first object containing a given property with a matching value
+  /*
+  find : function(collection, attr, val) {
+    return _.detect(collection, function (obj) {
+      return obj[attr] == val;
+    });
+  },
+  */
+
+  // deep copies an object using jQuery's extend
+  copy: function(obj) {
+    return $.extend(true, {}, obj);
+  },
+
+  // inserts a string into another string at a given index and returns a copy
+  insert: function(str, strToInsert, index) {
+    return str.substr(0,index) + strToInsert + str.substr(index);
+  },
+
+  // dist formula
+  distance: function (point1, point2) {
+    var xs = point2.x - point1.x;
+    var ys = point2.y - point1.y;
+    return Math.sqrt(xs*xs + ys*ys);
   },
   
 });
